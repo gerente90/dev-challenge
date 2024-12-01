e67361ceee9f26d45caa8aeef6afc4e7e53ba606
Elkin Guamán <edguamanc@puce.edu.ec>
2024-11-30 23:46:18 -0500

    Upgrades

---
+++ script.js
@@ -0,0 +1,9 @@
+function saveUser() {
+    botonClick = document.getElementById("save");
+    botonClick.addEventListener("click", () => {
+        mensaje.style.display = "block";
+
+    })
+};
+
+saveUser();
